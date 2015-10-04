5366f796054f205120d09813d8e28710c2e5293a
Tim Bradshaw <tim@alltrips.com>
2015-10-04 17:34:58 -0400

    Add Login and Authorization Controllers

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
+    return Auth::user();
     return view('welcome');
 });
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
