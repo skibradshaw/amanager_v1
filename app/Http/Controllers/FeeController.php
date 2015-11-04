960a88b7111e200a66ee7f7706c4a0120725b602
Tim Bradshaw <tim@alltrips.com>
2015-11-04 09:30:35 -0500

    Add auth middleware to all controllers

--- app/Http/Controllers/FeeController.php
+++ app/Http/Controllers/FeeController.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\Controller;
 
 class FeeController extends Controller
 {
+    public function __construct()
+    {
+	    $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
