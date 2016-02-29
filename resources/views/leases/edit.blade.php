cf62f7cc84f5b09d09979941710f1a81cc0da375
Tim Bradshaw <tim@alltrips.com>
2016-02-28 19:55:12 -0700

    Fix Create Lease layout on Mobile

--- resources/views/leases/edit.blade.php
+++ resources/views/leases/edit.blade.php
@@ -11,90 +11,113 @@
 		{!! Form::open(['route' => ['apartments.lease.store',$apartment->name]]) !!}
 	@endif
 	{!! Form::hidden('apartment_id',$apartment->id) !!}
-	<div class="row collapse">
-		<div class="row">
-			<div class="small-1 columns">
-				{!! Form::label('startdate','Start:',['class' => 'inline']) !!} 
-			</div>
-			<div class="small-2 columns left">
-				{!! Form::text('startdate',null,['id' => 'datepicker','placeholder' => 'mm/dd/yyyy', 'style' => 'position: relative; z-index: 100000;']) !!}
-			</div>
-			<div class="small-1 columns">
-				{!! Form::label('enddate','End:',['class' => 'inline']) !!} 
-			</div>			
-			<div class="small-2 columns end">
-				
-				{!! Form::text('enddate',null,['id' => 'datepicker1','placeholder' => 'mm/dd/yyyy', 'style' => 'position: relative; z-index: 100000;']) !!}
-			</div>
-			<div class="small-6 columns">
+		<div class="row collapse">
+			<div class="medium-6 columns">
 				 {!! $errors->first('startdate','<span class="label alert radius">:message</span>') !!}
+			</div>	
+		</div>
+		<div class="row collapse">
+		    <div class="small-3 large-2 columns">
+		      <span class="prefix">{!! Form::label('startdate','Lease Start:',['class' => 'inline']) !!}</span>
+		    </div>
+		    <div class="small-4 large-2 end columns">
+		      {!! Form::text('startdate',null,['id' => 'datepicker','placeholder' => 'mm/dd/yyyy', 'style' => 'position: relative; z-index: 100000;']) !!}
+		    </div>
+		</div>
+		<div class="row collapse">
+			<div class="medium-6 columns">
 				 {!! $errors->first('enddate','<span class="label alert radius">:message</span>') !!}
 			</div>	
 		</div>
 		<div class="row collapse">
-			
-			<div class="small-2 columns">
-				{!! Form::label('monthly_rent','Monthly Rent: ') !!}
-			</div>
-	       <div class="small-1 columns">
-	          <span class="prefix">$</span>
-	        </div>	    
-			<div class="small-2 columns end">
-				{!! Form::text('monthly_rent') !!}
-			</div>
-			<div class="small-7 columns end">
-				{!! $errors->first('monthly_rent','<span class="label alert radius">:message</span>') !!}
-			</div>
-
+		    <div class="small-3 large-2 columns">
+		      <span class="prefix">{!! Form::label('enddate','Lease End:',['class' => 'inline']) !!}</span>
+		    </div>
+		    <div class="small-4 large-2 end columns">
+		     {!! Form::text('enddate',null,['id' => 'datepicker1','placeholder' => 'mm/dd/yyyy', 'style' => 'position: relative; z-index: 100000;']) !!}
+		    </div>		
 		</div>
 		<div class="row collapse">
-			<div class="small-2 columns">
-				{!! Form::label('pet_rent','Pet Rent: ') !!}
-			</div>
-	       <div class="small-1 columns">
-	          <span class="prefix">$</span>
-	        </div>	    
-			<div class="small-2 columns end">
-				{!! Form::text('pet_rent') !!}
-			</div>
-			<div class="small-7 columns end">
-				{!! $errors->first('pet_rent','<span class="label alert radius">:message</span>') !!}
-			</div>
+			<div class="medium-6 columns">
+				 {!! $errors->first('monthly_rent','<span class="label alert radius">:message</span>') !!}
+			</div>	
+		</div>
+		    
+		<div class="row">
+		    <div class="small-3 large-2 columns">
+		      {!! Form::label('monthly_rent','Monthly Rent:',['class' => 'inline']) !!}
+		    </div>
+		    <div class="small-4 large-2 columns end">
+		    	<div class="row collapse">
+			       <div class="small-3 columns">
+			          <span class="prefix">$</span>
+			       </div>
+			       <div class="small-9 columns end">
+			       	  {!! Form::text('monthly_rent') !!}
+			       </div>	 		    		
+		    	</div>		      
+		    </div>
 		</div>
 		<div class="row collapse">
-			<div class="small-2 columns">
-				{!! Form::label('deposit','Deposit Amount: ') !!}
-			</div>
-	       <div class="small-1 columns">
-	          <span class="prefix">$</span>
-	        </div>	    
-
-			<div class="small-2 columns end">
-				{!! Form::text('deposit') !!}
-			</div>
-			<div class="small-7 columns end">
-				{!! $errors->first('deposit','<span class="label alert radius">:message</span>') !!}
-			</div>
-
+			<div class="medium-6 columns">
+				 {!! $errors->first('pet_rent','<span class="label alert radius">:message</span>') !!}
+			</div>	
+		</div>
+		<div class="row">
+		    <div class="small-3 large-2 columns">
+		      {!! Form::label('pet_rent','Pet Rent:',['class' => 'inline']) !!}
+		    </div>
+		    <div class="small-4 large-2 columns end">
+		    	<div class="row collapse">
+			       <div class="small-3 columns">
+			          <span class="prefix">$</span>
+			       </div>
+			       <div class="small-9 columns end">
+			       	  {!! Form::text('pet_rent') !!}
+			       </div>	 		    		
+		    	</div>		      
+		    </div>
 		</div>
 		<div class="row collapse">
-			<div class="small-2 columns">
-				{!! Form::label('pet_deposit','Pet Deposit: ') !!}
-			</div>
-	       <div class="small-1 columns">
-	          <span class="prefix">$</span>
-	        </div>	    
-
-			<div class="small-2 columns end">
-				{!! Form::text('pet_deposit') !!}
-			</div>
-			<div class="small-7 columns end">
-				{!! $errors->first('pet_deposit','<span class="label alert radius">:message</span>') !!}
-			</div>
+			<div class="medium-6 columns">
+				 {!! $errors->first('deposit','<span class="label alert radius">:message</span>') !!}
+			</div>	
 		</div>
-
-	</div>
-	
+		<div class="row">
+		    <div class="small-3 large-2 columns">
+		      {!! Form::label('deposit','Deposit Amount:',['class' => 'inline']) !!}
+		    </div>
+		    <div class="small-4 large-2 columns end">
+		    	<div class="row collapse">
+			       <div class="small-3 columns">
+			          <span class="prefix">$</span>
+			       </div>
+			       <div class="small-9 columns end">
+			       	  {!! Form::text('deposit') !!}
+			       </div>	 		    		
+		    	</div>		      
+		    </div>
+		</div>
+		<div class="row collapse">
+			<div class="medium-6 columns">
+				 {!! $errors->first('pet_deposit','<span class="label alert radius">:message</span>') !!}
+			</div>	
+		</div>
+		<div class="row">
+		    <div class="small-3 large-2 columns">
+		      {!! Form::label('pet_deposit','Pet Deposit:',['class' => 'inline']) !!}
+		    </div>
+		    <div class="small-4 large-2 columns end">
+		    	<div class="row collapse">
+			       <div class="small-3 columns">
+			          <span class="prefix">$</span>
+			       </div>
+			       <div class="small-9 columns end">
+			       	  {!! Form::text('pet_deposit') !!}
+			       </div>	 		    		
+		    	</div>		      
+		    </div>
+		</div>	
 	@if(isset($lease))
 		<button type="submit" class="radius button">Update</button>
 		
