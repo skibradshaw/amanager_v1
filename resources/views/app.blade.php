839f082053b56c8c747c0a7854d2884b0b34c3e3
Tim Bradshaw <tim@alltrips.com>
2016-03-06 12:20:38 -0700

    Adds Usersnap Feedback

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,7 +25,17 @@
         </div>
       </div>
 
-     
+<script type="text/javascript">
+(function() {
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.async = true;
+s.src = '//api.usersnap.com/load/'+
+        'b3bb63b8-89ec-49df-ad29-a67532c736d1.js';
+var x = document.getElementsByTagName('script')[0];
+x.parentNode.insertBefore(s, x);
+})();
+</script>     
   </body>
 
 </html>
