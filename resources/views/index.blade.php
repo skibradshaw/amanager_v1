bc4a71748d988c7ed750980a39f36c084c518217
Tim Bradshaw <tim@alltrips.com>
2016-03-06 12:11:58 -0700

    Fixes home page links

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
 	<h4>{{$p->name}} Overview</h4>
 	<div class="row">
 		<div class="large-3 columns">
-			<a href="{{ route('reports.rentsdue') }}">
+			<a href="{{ route('reports.rentsdue',['id' => $p->id]) }}">
 			<div class="panel text-center radius">
 				<h1><i class="fa fa-usd fa-1x"></i></h1>
 				<h4>Rents Due<br>${{ number_format($p->rentBalance()) }}</h4>
@@ -17,7 +17,7 @@
 			</a>
 		</div>
 		<div class="large-3 columns">
-			<a href="{{ route('reports.depositsdue') }}">
+			<a href="{{ route('reports.depositsdue',['id' => $p->id]) }}">
 			<div class="panel text-center radius">
 				<h1><i class="fa fa-lock fa-1x"></i></h1>
 				<h4>Deposits Due<br>${{ number_format($p->depositBalance()) }}</h4>
@@ -25,7 +25,7 @@
 			</a>
 		</div>
 		<div class="large-3 columns end">
-			<a href="{{ route('tenants.index') }}">
+			<!-- <a href="{{ route('tenants.index',['id' => $p->id]) }}"> -->
 			<div class="panel text-center radius">
 				<h1><i class="fa fa-user fa-1x"></i></h1>
 				<h4>Tenants<br>{{ number_format($p->tenants()) }}</h4>
@@ -33,7 +33,7 @@
 			</a>
 		</div>
 		<div class="large-3 columns end">
-			<a href="{{ route('properties.apartments.index') }}">
+			<!-- <a href="{{ route('properties.apartments.index') }}"> -->
 			<div class="panel text-center radius">
 				<h1><i class="fa fa-building-o fa-1x"></i></h1>
 				<h4>Apartments<br>{{ number_format($p->apartments()->count('apartments.id')) }}</h4>
